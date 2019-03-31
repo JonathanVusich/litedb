403561a8cff8c5720abfc3e9cf51e17a306a50ff
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-03-30 22:12:22 -0700

    Update object_utils.py

--- autodb/utils/object_utils.py
+++ autodb/utils/object_utils.py
@@ -4,10 +4,18 @@ acceptable_indexes = (int, bool, float, str, bytes, bytearray)
 
 
 def retrieve_object_indexes(complex_object: object) -> Dict[str, Union[int, bool, float, str, bytes, bytearray]]:
+    """
+    This method retrieves public variables of a class
+    that can be used to build indexes.
+    :param complex_object:
+    :return:
+    """
+
     indexes: Dict[str, Union[int, bool, float, str, bytes, bytearray]] = {}
     object_vars = vars(complex_object)
     for var in object_vars:
-        var_value = getattr(complex_object, var)
-        if isinstance(var_value, acceptable_indexes):
-            indexes.update({var: var_value})
+        if not var.startswith("_"):
+            var_value = getattr(complex_object, var)
+            if isinstance(var_value, acceptable_indexes):
+                indexes.update({var: var_value})
     return indexes
