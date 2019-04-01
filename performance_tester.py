5979785c6d9dc31a0acb9ea8e785454da4eb5a01
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-04-01 02:19:11 -0500

    Added ad hoc performance testing script

--- performance_tester.py
+++ performance_tester.py
@@ -1,11 +1,15 @@
 import time
-from autodb.index.index import Index
+from autodb.index import Index
+from autodb.table import Table
+from tests.test_utils.objects import ObjectClassVars
 
 def main():
-    index = Index(int)
+    item = ObjectClassVars()
+    table = Table(item)
     start = time.perf_counter()
-    for x in range(1000000):
-        index.create(x, x)
+    for i in range(1000000):
+        item = ObjectClassVars()
+        table.insert(item)
     print(time.perf_counter() - start)
 
 
