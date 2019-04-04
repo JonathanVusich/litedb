d2a929967e1798d25cc801405e174459b8f05453
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-04-04 01:48:58 -0500

    Update test_table.py

--- tests/test_table/test_table.py
+++ tests/test_table/test_table.py
@@ -45,24 +45,66 @@ def test_table_insert_objects():
     assert len(table.index_blacklist) == 0
 
 
-def test_table_retrieve_objects():
+def test_table_retrieve_well_formed_queries():
     table = Table()
 
     for i in range(10):
         table.insert(StandardTableObject(i, -i))
 
-    items = table.retrieve(x=9)
-    assert len(items) == 1
-    assert isinstance(items[0], StandardTableObject)
-    assert items[0].x == 9
+    # test x queries
 
-    assert not table.retrieve(x=10)
+    indexes = table._retrieve(x=9)
+    assert len(indexes) == 1
+    sto = table.table[indexes.pop()]
+    assert isinstance(sto, StandardTableObject)
+    assert sto.x == 9
 
-    items = table.retrieve(x=(1, 3))
-    assert len(items) == 3
-    for x, item in enumerate(items):
-        assert isinstance(item, StandardTableObject)
-        assert item.x == x + 1
-        assert item.y == -(x + 1)
+    assert not table._retrieve(x=10)
+
+    indexes = table._retrieve(x=(1, 3))
+    assert len(indexes) == 3
+    for x, item in enumerate(indexes):
+        sto = table.table[item]
+        assert isinstance(sto, StandardTableObject)
+        assert sto.x == x + 1
+        assert sto.y == -(x + 1)
+
+    # test y queries
+
+    indexes = table._retrieve(y=-9)
+    assert len(indexes) == 1
+    sto = table.table[indexes.pop()]
+    assert isinstance(sto, StandardTableObject)
+    assert sto.y == -9
+
+    assert not table._retrieve(y=-10)
+
+    indexes = table._retrieve(y=(-3, -1))
+    assert len(indexes) == 3
+    for x, item in enumerate(indexes):
+        sto = table.table[item]
+        assert isinstance(sto, StandardTableObject)
+        assert sto.x == x + 1
+        assert sto.y == -(x + 1)
+
+    # verify no references returned
+    item = table.retrieve(x=1)[0]
+    item.x = 2
+    assert table.retrieve(x=1)[0].x == 1
+
+
+def test_table_retrieve_bad_queries():
+    table = Table()
+
+    for i in range(10):
+        table.insert(StandardTableObject(i, -i))
+
+    with pytest.raises(IndexError):
+        table._retrieve(z=12)
+
+    with pytest.raises(IndexError):
+        table._retrieve(x=(1, 3, 5))
+
+
 
 
