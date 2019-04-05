0c5b5c982df436cedf007629150679811eb0d448
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-04-05 01:08:22 -0500

    Added better errors to table

--- autodb/table/table.py
+++ autodb/table/table.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
-from typing import List, Set, Dict, Optional
+from typing import List, Set, Dict, Optional, Generator
 
 from ..index import Index
 from ..utils.object_utils import retrieve_possible_object_indexes
+from ..errors import InvalidRange
 
 
 class Table:
@@ -35,16 +36,17 @@ class Table:
         self.size += 1
         self._index_item(item, index)
 
-    def retrieve(self, **kwargs) -> Optional:
+    def retrieve(self, **kwargs) -> Optional[Generator[object, None, None]]:
         indexes = self._retrieve(**kwargs)
         if indexes:
-            retrieved_items = [deepcopy(self.table[index]) for index in indexes]
+            retrieved_items = (deepcopy(self.table[index]) for index in indexes)
             return retrieved_items
 
     def delete(self, **kwargs):
         indexes_to_delete = self._retrieve(**kwargs)
-        for index in indexes_to_delete:
-            self._delete(index)
+        if indexes_to_delete:
+            for index in indexes_to_delete:
+                self._delete(index)
 
     def _index_item(self, item: object, index: int) -> None:
         """Inserts/creates index tables based on the given object."""
@@ -72,26 +74,26 @@ class Table:
         indexes: Set[int] = set()
         for x, key in enumerate(kwargs.keys()):
             if key in self.index_blacklist or key not in self.index_map:
-                raise IndexError
+                raise IndexError(f"{key} is not a valid index!")
             index = self.index_map[key]
             if len(index) == 0:
                 continue
             value = kwargs[key]
             if isinstance(value, tuple):
                 if len(value) != 2:
-                    raise IndexError
+                    raise InvalidRange
                 low, high = value
                 if low is not None and not isinstance(low, index.index_type):
-                    raise IndexError
+                    raise ValueError(f"The low value of \"{key}\" must be of type {index.index_type}")
                 if high is not None and not isinstance(high, index.index_type):
-                    raise IndexError
+                    raise ValueError(f"The high value of \"{key}\" must be of type {index.index_type}")
                 if x == 0:
                     indexes.update(index.retrieve_range(low, high))
                 else:
                     indexes.intersection_update(index.retrieve_range(low, high))
             else:
                 if value is not None and not isinstance(value, index.index_type):
-                    raise IndexError
+                    raise ValueError(f"\"{key}\" must be of type {index.index_type}")
                 results = index.retrieve(value)
                 if results:
                     if x == 0:
