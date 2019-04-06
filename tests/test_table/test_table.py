63904ef9596ae2fb89755c6d6528f81c631f346e
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-04-06 12:01:45 -0500

    Added a table test

--- tests/test_table/test_table.py
+++ tests/test_table/test_table.py
@@ -122,6 +122,12 @@ def test_table_retrieve_well_formed_queries():
     item.x = 2
     assert list(table.retrieve(x=1))[0].x == 1
 
+    # verify that no kwargs returns all objects
+    items = list(table.retrieve())
+    assert len(items) == 10
+    for item in items:
+        assert isinstance(item, StandardTableObject)
+
 
 def test_table_retrieve_bad_queries():
     table = Table()
