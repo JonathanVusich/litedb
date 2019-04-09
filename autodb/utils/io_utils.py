b3f7d99dfb6c81c1fff30c72914d402a01e38580
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-04-09 00:14:29 -0500

    Added io_utils for saving and loading database files

--- autodb/utils/io_utils.py
+++ autodb/utils/io_utils.py
@@ -1,10 +1,70 @@
-from os import scandir
+from os import scandir, listdir, DirEntry
+from typing import List, Generator, Tuple
+
+from ..errors import DatabaseNotFound
 
 
 def dir_empty(directory: str) -> bool:
     # Check that the path is an empty directory
     with scandir(path=directory) as curdir:
         for entry in curdir:
-            if entry.is_file() or entry.is_dir():
+            if entry.is_file(follow_symlinks=False) or entry.is_dir(follow_symlinks=False):
                 return False
+    return True
+
+
+def load_database(directory: str) -> Generator[Tuple[str, str, List[str]], None, None]:
+    tables = 0
+    with scandir(path=directory) as curdir:
+        for entry in curdir:
+            if is_table_folder(entry):
+                tables += 1
+                yield retrieve_table_from_directory(entry)
+    if tables == 0:
+        raise DatabaseNotFound
+
+
+def retrieve_table_from_directory(directory: DirEntry) -> Tuple[str, str, List[str]]:
+    table_path = directory.path
+    index = None
+    shards = []
+    with scandir(path=table_path) as curdir:
+        for entry in curdir:
+            if is_index_file(entry):
+                index = entry.path
+            elif is_shard_file(entry):
+                shards.append(entry.path)
+    return table_path, index, shards
+
+
+def is_table_folder(directory: DirEntry) -> bool:
+    return directory.is_dir(follow_symlinks=False) and is_table(directory.name) and valid_table_contents(directory.path)
+
+
+def is_table(file_name: str) -> bool:
+    return file_name.startswith("table") and len(file_name) > 5 and file_name[5:].isdigit()
+
+
+def is_shard_file(file: DirEntry) -> bool:
+    return is_shard(file.name) and file.is_file(follow_symlinks=False)
+
+
+def is_index_file(file: DirEntry) -> bool:
+    return is_index(file.name) and file.is_file(follow_symlinks=False)
+
+
+def is_shard(file_name: str) -> bool:
+    return file_name.startswith("shard") and len(file_name) > 5 and file_name[5:].isdigit()
+
+
+def is_index(file_name: str) -> bool:
+    return file_name == "index"
+
+
+def valid_table_contents(dir_path: str) -> bool:
+    files = [file for file in listdir(dir_path)]
+    index_files = [file for file in files if is_index(file)]
+    shard_files = [file for file in files if is_shard(file)]
+    if len(index_files) < 1 or len(shard_files) < 1:
+        return False
     return True
---
+++ tests/test_utils/test_io_utils.py
@@ -0,0 +1,118 @@
+import os
+import tempfile
+import pytest
+
+from autodb.errors import DatabaseNotFound
+from autodb.utils.io_utils import dir_empty, load_database, is_shard, is_index, is_table, \
+    valid_table_contents
+
+
+def test_dir_empty():
+    with tempfile.TemporaryDirectory() as tempdir:
+        with tempfile.NamedTemporaryFile(dir=tempdir) as tmp:
+            assert not dir_empty(tempdir)
+        assert dir_empty(tempdir)
+
+
+def test_load_database_well_formed():
+    with tempfile.TemporaryDirectory() as tempdir:
+        table_dir = os.path.join(tempdir, "table1")
+        os.mkdir(table_dir)
+        index_path = os.path.join(table_dir, "index")
+        with open(index_path, "wb"):
+            pass
+        shard0_path = os.path.join(table_dir, "shard0")
+        with open(shard0_path, "wb"):
+            pass
+        tables = list(load_database(tempdir))
+        assert len(tables) == 1
+        table = tables[0]
+        assert table[0] == table_dir
+        assert table[1] == index_path
+        assert len(table[2]) == 1
+        assert table[2][0] == shard0_path
+
+
+def test_load_database_with_junk_files():
+    with tempfile.TemporaryDirectory() as tempdir:
+        table_dir = os.path.join(tempdir, "table1")
+        os.mkdir(table_dir)
+
+        # Add junk table directories
+        os.mkdir(os.path.join(tempdir, "table1234d"))
+        os.mkdir(os.path.join(tempdir, "table2df21"))
+
+        index_path = os.path.join(table_dir, "index")
+        with open(index_path, "wb"):
+            pass
+
+        # Add junk index file
+        with open(os.path.join(table_dir, "index12"), "wb"):
+            pass
+
+        shard0_path = os.path.join(table_dir, "shard0")
+        with open(shard0_path, "wb"):
+            pass
+
+        # Add junk shards
+        with open(os.path.join(table_dir, "shardd1"), "wb"):
+            pass
+        with open(os.path.join(table_dir, "shard2g"), "wb"):
+            pass
+
+        tables = list(load_database(tempdir))
+        assert len(tables) == 1
+        table = tables[0]
+        assert table[0] == table_dir
+        assert table[1] == index_path
+        assert len(table[2]) == 1
+        assert table[2][0] == shard0_path
+
+
+def test_load_database_no_database():
+    with tempfile.TemporaryDirectory() as tempdir:
+        table_dir = os.path.join(tempdir, "table1")
+        os.mkdir(table_dir)
+        shard0_path = os.path.join(table_dir, "shard0")
+        with open(shard0_path, "wb"):
+            pass
+        with pytest.raises(DatabaseNotFound):
+            list(load_database(tempdir))
+
+
+def test_is_table():
+    assert is_table("table01")
+
+
+def test_is_shard():
+    assert is_shard("shard01")
+    assert not is_shard("shard_01")
+    assert is_shard("shard12345")
+    assert not is_shard("shard")
+
+
+def test_is_index():
+    assert is_index("index")
+    assert not is_index("index1")
+
+
+def test_is_valid_table_directory():
+    with tempfile.TemporaryDirectory() as tempdir:
+        with open(os.path.join(tempdir, "index"), "wb"):
+            pass
+        with open(os.path.join(tempdir, "shard0"), "wb"):
+            pass
+
+        assert valid_table_contents(tempdir)
+
+        # remove index file
+        os.remove(os.path.join(tempdir, "index"))
+
+        assert not valid_table_contents(tempdir)
+
+        # Add index file back and remove shard file
+        with open(os.path.join(tempdir, "index"), "wb"):
+            pass
+        os.remove(os.path.join(tempdir, "shard0"))
+
+        assert not valid_table_contents(tempdir)
