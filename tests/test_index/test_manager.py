59c889526b613bdf376b2f9facd64708d175cdd2
Jonathan Vusich <apianist16@gmail.com>
2019-04-18 11:40:38 -0500

    Added index_manager retrieval tests

--- tests/test_index/test_manager.py
+++ tests/test_index/test_manager.py
@@ -2,7 +2,8 @@ import pytest
 from autodb.index.manager import IndexManager
 from autodb.index.index import Index
 import pickle
-from ..test_table.table_test_objects import StandardTableObject
+from ..test_table.table_test_objects import StandardTableObject, BadObject
+from autodb.errors import InvalidRange
 
 
 @pytest.fixture()
@@ -47,7 +48,7 @@ def test_persist_load(table_dir, index):
     assert new_manager.index_map == {"test": index}
 
 
-def test_index_item(index_manager):
+def test_index_item(index_manager, table_dir):
     sto = StandardTableObject(12, -12)
     index_manager.index_item(sto, 12)
     assert index_manager.index_blacklist == set()
@@ -56,6 +57,23 @@ def test_index_item(index_manager):
     index2 = Index(int)
     index2.add(-12, 12)
     assert index_manager.index_map == {"x": index1, "y": index2}
+
+    # Clear out the old index
+    index_manager = IndexManager(str(table_dir.join("index")))
+
+    # test index blacklist
+    bo = BadObject(12)
+    bo2 = BadObject(13)
+    index_manager.index_item(bo, 12)
+    assert index_manager.index_blacklist == set()
+    assert 'bad_index' in index_manager.index_map
+    index_manager.index_item(bo2, 13)
+    assert index_manager.index_blacklist == {'bad_index'}
+    assert index_manager.index_map == {}
+    bo3 = BadObject(14)
+    index_manager.index_item(bo3, 14)
+    assert index_manager.index_blacklist == {'bad_index'}
+    assert index_manager.index_map == {}
 
 
 def test_unindex_item(index_manager):
@@ -66,4 +84,61 @@ def test_unindex_item(index_manager):
     index1 = Index(int)
     index2 = Index(int)
     assert index_manager.index_map == {"x": index1, "y": index2}
+
+
+def test_table_retrieve_well_formed_queries(index_manager):
+    for i in range(10):
+        index_manager.index_item(StandardTableObject(i, -i), i)
+
+    # test x queries
+
+    indexes = index_manager.retrieve(x=9)
+    assert len(indexes) == 1
+    assert 9 in indexes
+
+    assert not index_manager.retrieve(x=10)
+
+    indexes = index_manager.retrieve(x=(1, 3))
+    assert len(indexes) == 3
+    assert indexes == {1, 2, 3}
+
+    # test y queries
+
+    indexes = index_manager.retrieve(y=-9)
+    assert len(indexes) == 1
+    assert indexes == {9}
+
+    assert not index_manager.retrieve(y=-10)
+
+    indexes = index_manager.retrieve(y=(-3, -1))
+    assert len(indexes) == 3
+    assert indexes == {1, 2, 3}
+
+    # Check multiple queries
+    items = index_manager.retrieve(x=9, y=-9)
+    assert len(items) == 1
+    assert items == {9}
+
+    assert not index_manager.retrieve(x=9, y=9)
+
+
+def test_table_retrieve_bad_queries(index_manager):
+
+    for i in range(10):
+        index_manager.index_item(StandardTableObject(i, -i), i)
+
+    with pytest.raises(IndexError):
+        index_manager.retrieve(z=12)
+
+    with pytest.raises(InvalidRange):
+        index_manager.retrieve(x=(1, 3, 5))
+
+    with pytest.raises(ValueError):
+        index_manager.retrieve(x=b"test")
+
+    with pytest.raises(ValueError):
+        index_manager.retrieve(x=(b"test", b"test2"))
+
+    with pytest.raises(ValueError):
+        index_manager.retrieve(x=(1, b"test"))
 
