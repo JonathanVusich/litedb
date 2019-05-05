9e1b053704036cab4fb7b0c93f714c5dcfba11ca
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-05-05 17:08:28 -0500

    Updated base classes for table

--- autodb/table/table.py
+++ autodb/table/table.py
@@ -20,5 +20,17 @@ class Table(ABC):
         raise NotImplemented
 
     @abstractmethod
+    def retrieve_all(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def retrieve_valid_indexes(self):
+        raise NotImplemented
+
+    @abstractmethod
     def delete(self, **kwargs):
+        raise NotImplemented
+
+    @abstractmethod
+    def delete_all(self):
         raise NotImplemented
