d41b3925ec23ac8f105baa677393dedbfe2183f9
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-05-05 17:13:31 -0500

    Implement new database methods

--- autodb/database/disk_database.py
+++ autodb/database/disk_database.py
@@ -1,10 +1,11 @@
 import os
-from typing import Dict
+from typing import Dict, Generator
 
 from .database import Database
 from ..errors import DatabaseNotFound
 from ..table import PersistentTable
 from ..utils.path import load_tables
+from ..utils.io import rmdir
 
 
 class DiskDatabase(Database):
@@ -56,14 +57,34 @@ class DiskDatabase(Database):
                                                           table_type=first_item_type)})
                 self.tables[first_item_type].batch_insert(items)
 
-    def retrieve(self, class_type=None, **kwargs):
+    def retrieve(self, cls, **kwargs):
         try:
-            return self.tables[class_type].retrieve(**kwargs)
+            return self.tables[cls].retrieve(**kwargs)
         except KeyError:
             raise KeyError("No objects of this type were ever stored in this database!")
 
-    def delete(self, class_type=None, **kwargs):
+    def retrieve_all(self) -> Generator[object, None, None]:
+        for table in self.tables.values():
+            for cls in table.retrieve_all():
+                yield cls
+
+    def retrieve_valid_indexes(self, cls):
         try:
-            return self.tables[class_type].delete(**kwargs)
+            return self.tables[cls].retrieve_valid_indexes()
+        except KeyError:
+            raise KeyError("No objects of this type are in this database!")
+
+    def delete(self, cls, **kwargs):
+        try:
+            return self.tables[cls].delete(**kwargs)
         except KeyError:
             raise KeyError("No objects of this type were ever stored in this database!")
+
+    def clear(self):
+        with os.scandir(self.directory) as dir_contents:
+            for entry in dir_contents:
+                if entry.is_file():
+                    os.unlink(entry)
+                if entry.is_dir():
+                    rmdir(entry)
+        self.tables = {}
