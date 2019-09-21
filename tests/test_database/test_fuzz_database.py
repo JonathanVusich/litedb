c578f96f538d3bdb61f2fc27b38478a0eca0596e
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-09-21 16:57:12 -0500

    Remove test data gathering from fuzz test

--- tests/test_database/test_fuzz_database.py
+++ tests/test_database/test_fuzz_database.py
@@ -10,28 +10,8 @@ from autodb.abc import Database
 
 @fixture
 def test_data():
-    test_data = "fuzz_test"
-    if not os.path.exists(test_data):
-        api = API()
-        data = api.retrieve_all()
-        parts = {}
-        values = list(data.values())
-        for i in range(5000):
-            test_parts: list = choice(values)
-            while not test_parts:
-                test_parts = choice(values)
-            part = choice(test_parts)
-            part_key = type(part).__name__
-            if part_key in parts:
-                parts[part_key].add(part)
-            else:
-                parts[part_key] = {part}
-        with open(test_data, "wb") as file:
-            pickle.dump(parts, file)
-            return parts
-    else:
-        with open(test_data, "rb") as file:
-            return pickle.load(file)
+    with open("fuzz_test", "rb") as file:
+        return pickle.load(file)
 
 
 @fixture
@@ -44,9 +24,6 @@ def format_indexes(indexes: list, item: object) -> dict:
 
 
 def test_fuzz_disk(test_data, database_dir: str):
-    profiler = cProfile.Profile()
-    profiler.enable()
-
     database = DiskDatabase(database_dir)
 
     for key, parts in test_data.items():
@@ -66,9 +43,6 @@ def test_fuzz_disk(test_data, database_dir: str):
     database = DiskDatabase(database_dir)
 
     assert len(database) == 0
-
-    profiler.disable()
-    profiler.dump_stats("test2.prof")
 
 
 def test_fuzz_memory(test_data):
